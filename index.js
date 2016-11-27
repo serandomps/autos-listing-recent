a92056866235b425c1135c98b5878f680cb1475f
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2016-11-28 01:44:29 +0530

    updating

---
+++ index.js
@@ -0,0 +1,20 @@
+var dust = require('dust')();
+var serand = require('serand');
+var utils = require('utils');
+var Vehicle = require('vehicle-service');
+var list = require('autos-listing');
+
+module.exports = function (sandbox, fn, options) {
+    Vehicle.find({
+        query: options,
+        images: '288x162'
+    }, function (err, vehicles) {
+        if (err) {
+            return fn(true, serand.none);
+        }
+        list(sandbox, fn, {
+            vehicles: vehicles,
+            title: 'Recently Added'
+        });
+    });
+};
