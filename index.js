7d8a13ef3929b610d3f524f3eebf041ce0a2524a
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-02-06 19:28:18 +0530

    updating

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function (ctx, container, options, done) {
     var sandbox = container.sandbox;
     Vehicle.find({
         query: options,
-        images: '288x162'
+        resolution: '288x162'
     }, function (err, vehicles) {
         if (err) {
             return done(err);
